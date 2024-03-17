46cf73e3099b26ca909e909ac85baaac871f957a
Ryo Nakai <50983271+sniper-fly@users.noreply.github.com>
2024-03-17 17:28:18 +0900

    publicにimagesを移動

--- src/app/base-apparel-coming-soon-master/page.tsx
+++ src/app/base-apparel-coming-soon-master/page.tsx
@@ -1,18 +1,38 @@
 // import styles from "./styles.module.css";
-import bg from "./images/bg-pattern-desktop.svg"
+import Image from "next/image";
 
 export default function Page() {
+  const prefix = "/base-apparel-coming-soon-master/";
+
   return (
-    <div className="flex">
-      {/* <div className={`bg-fixed ${styles.bg_wallpaper}`}>
+    <>
+      <div className="flex">
+        {/* <div className={`bg-fixed ${styles.bg_wallpaper}`}>
         <div></div>
       </div> */}
-      <div
-        className="bg-cover bg-center w-full h-screen"
-        style={{ backgroundImage: `url(${bg.src})` }}
-      >
-        {/* ここに他のコンポーネントやコンテンツを追加できます */}
+        <div
+          className="bg-cover w-3/5 h-screen"
+          style={{
+            backgroundImage: `url(${prefix}/bg-pattern-desktop.svg)`,
+          }}
+        >
+          <div className="px-10 my-8 md:mb-20 md:mt-12">
+            <img
+              src={`${prefix}/logo.svg`}
+              alt="logo"
+              className="w-24 md:w-36"
+            />
+            {/* <Image src="/base-apparel-coming-soon-master/logo.svg" fill={true} alt="logo" /> */}
+          </div>
+        </div>
+
+        <div
+          className="bg-cover w-2/5 h-screen"
+          style={{
+            backgroundImage: `url(${prefix}/hero-desktop.jpg)`,
+          }}
+        ></div>
       </div>
-    </div>
+    </>
   );
 }
