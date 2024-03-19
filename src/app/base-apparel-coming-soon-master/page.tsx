703d5bcc4583957ae9c6e152a1f15166f3b115b7
Ryo Nakai <50983271+sniper-fly@users.noreply.github.com>
2024-03-19 17:39:28 +0900

    ロゴの表示

--- src/app/base-apparel-coming-soon-master/page.tsx
+++ src/app/base-apparel-coming-soon-master/page.tsx
@@ -2,34 +2,39 @@
 import Image from "next/image";
 
 export default function Page() {
-  const prefix = "/base-apparel-coming-soon-master/";
+  const imagePath = "/base-apparel-coming-soon-master/";
 
   return (
     <>
-      <div className="flex">
+      <div className="flex flex-row">
+        {/* <div> */}
         {/* <div className={`bg-fixed ${styles.bg_wallpaper}`}>
         <div></div>
       </div> */}
         <div
           className="bg-cover w-3/5 h-screen"
           style={{
-            backgroundImage: `url(${prefix}/bg-pattern-desktop.svg)`,
+            backgroundImage: `url(${imagePath}/bg-pattern-desktop.svg)`,
           }}
         >
-          <div className="px-10 my-8 md:mb-20 md:mt-12">
-            <img
-              src={`${prefix}/logo.svg`}
-              alt="logo"
-              className="w-24 md:w-36"
-            />
-            {/* <Image src="/base-apparel-coming-soon-master/logo.svg" fill={true} alt="logo" /> */}
+          {/* base apparerlから始まる一連の説明など */}
+          <div className="pl-40">
+            <div className="px-10 my-8 mb-20 mt-12">
+              <Image
+                src={`${imagePath}/logo.svg`}
+                width={36}
+                height={36}
+                className="w-36"
+                alt="logo"
+              />
+            </div>
           </div>
         </div>
 
         <div
           className="bg-cover w-2/5 h-screen"
           style={{
-            backgroundImage: `url(${prefix}/hero-desktop.jpg)`,
+            backgroundImage: `url(${imagePath}/hero-desktop.jpg)`,
           }}
         ></div>
       </div>
