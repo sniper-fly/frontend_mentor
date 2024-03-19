4fb4a6386243a9ef2293737a290af78fe8d76ea8
Ryo Nakai <50983271+sniper-fly@users.noreply.github.com>
2024-03-19 17:55:37 +0900

    説明文を追加 / 未スタイル

--- src/app/base-apparel-coming-soon-master/page.tsx
+++ src/app/base-apparel-coming-soon-master/page.tsx
@@ -19,6 +19,7 @@ export default function Page() {
         >
           {/* base apparerlから始まる一連の説明など */}
           <div className="pl-40">
+            {/* ロゴ */}
             <div className="px-10 my-8 mb-20 mt-12">
               <Image
                 src={`${imagePath}/logo.svg`}
@@ -27,6 +28,19 @@ export default function Page() {
                 className="w-36"
                 alt="logo"
               />
+            </div>
+            {/* ページ説明ブロック */}
+            <div className="text-left">
+              <h1 className="text-5xl">
+                <span className="block text-rose-400 font-thin">WE'RE</span>
+                <span className="block text-gray-800">COMING</span>
+                <span className="block text-gray-800">SOON</span>
+              </h1>
+              <p className="">
+                Hello fellow shoppers! We're currently building our new fashion
+                store. Add your email below to stay up-to-date with
+                announcements and our launch deals.
+              </p>
             </div>
           </div>
         </div>
