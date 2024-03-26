faada12a89cf651f37c71fb9d6106069dd213fa7
Ryo Nakai <50983271+sniper-fly@users.noreply.github.com>
2024-03-26 17:29:03 +0900

    input欄をあらかた骨格作成

--- src/app/base-apparel-coming-soon-master/page.tsx
+++ src/app/base-apparel-coming-soon-master/page.tsx
@@ -15,31 +15,61 @@ export default function Page() {
         >
           {/* base apparerlから始まる一連の説明など */}
           <div className="pl-40">
-            {/* ロゴ */}
-            <div className="px-10 my-8 mb-20 mt-12">
-              <Image
-                src={`${imagePath}/logo.svg`}
-                width={36}
-                height={36}
-                className="w-36"
-                alt="logo"
-              />
-            </div>
+            <div className="px-10">
 
-            {/* ページ説明ブロック */}
-            <div className="text-left px-10">
-              <h1 className="text-7xl tracking-widest mb-7">
-                <span className="block text-rose-800 font-thin">WE'RE</span>
-                <span className="block font-medium text-gray-800">COMING</span>
-                <span className="block font-medium text-gray-800">SOON</span>
-              </h1>
-              <p className="leading-relaxed text-rose-800">
-                Hello fellow shoppers! We're currently building our new fashion
-                store. Add your email below to stay up-to-date with
-                announcements and our launch deals.
-              </p>
-            </div>
+              {/* ロゴ */}
+              <div className="my-8 mb-20 mt-12">
+                <Image
+                  src={`${imagePath}/logo.svg`}
+                  width={36}
+                  height={36}
+                  className="w-36"
+                  alt="logo"
+                />
+              </div>
 
+              {/* ページ説明ブロック */}
+              <div className="text-left">
+                <h1 className="text-7xl tracking-widest mb-7">
+                  <span className="block text-rose-800 font-thin">WE'RE</span>
+                  <span className="block font-medium text-gray-800">
+                    COMING
+                  </span>
+                  <span className="block font-medium text-gray-800">SOON</span>
+                </h1>
+                <p className="leading-relaxed text-rose-800">
+                  Hello fellow shoppers! We're currently building our new
+                  fashion store. Add your email below to stay up-to-date with
+                  announcements and our launch deals.
+                </p>
+              </div>
+
+              {/* フォーム */}
+              <form className="flex mt-9 rounded-full shadow-rose-700 shadow">
+                <input
+                  type="text"
+                  className=" bg-transparent pl-6 py-3"
+                  placeholder="Email Address"
+                />
+                <Image
+                  src={`${imagePath}/icon-error.svg`}
+                  alt="error"
+                  width={20}
+                  height={20}
+                  className="w-5 h-5 my-auto mr-4"
+                />
+                <button className="w-28 bg-rose-200 rounded-full">
+                  <Image
+                    src={`${imagePath}/icon-arrow.svg`}
+                    alt="arrow"
+                    width={20}
+                    height={20}
+                    className="w-4 h-5 mx-auto"
+                  />
+                </button>
+              </form>
+
+            </div>
           </div>
         </div>
 
