0f8ae66b4e9c57fe62cbfdc7c93b374ae02e5d1a
Ryo Nakai <50983271+sniper-fly@users.noreply.github.com>
2024-04-11 16:46:03 +0900

    全体のレイアウトを作成

--- src/app/blog-preview-card-main/page.tsx
+++ src/app/blog-preview-card-main/page.tsx
@@ -1,3 +1,28 @@
+import Image from "next/image";
 
 export default function Page() {
+  const basePath = "/blog-preview-card-main/";
+
+  return (
+    <main className="flex h-screen items-center justify-center">
+      <section className="w-1/3 rounded-lg bg-white shadow-xl shadow-black">
+        {/* 画像 */}
+        <Image 
+          src={`${basePath}/illustration-article.svg`}
+          width={336}
+          height={201}
+          alt="illustration-editor-desktop"
+          className="rounded-lg m-3"
+        />
+        {/* type (ex. learning)  */}
+        {/* date */}
+        <h1>HTML & CSS foundations</h1>
+        <p>
+          These languages are the backbone of every website, defining structure,
+          content, and presentation.
+        </p>
+        {/* アイコン */}
+      </section>
+    </main>
+  );
 }
