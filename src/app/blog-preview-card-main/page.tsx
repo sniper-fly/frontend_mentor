97e7b8de7ec9b4ff5be60577627a9f680a57d30d
Ryo Nakai <50983271+sniper-fly@users.noreply.github.com>
2024-04-13 00:10:05 +0900

    文字・画像などを完全に用意

--- src/app/blog-preview-card-main/page.tsx
+++ src/app/blog-preview-card-main/page.tsx
@@ -1,12 +1,11 @@
 import Image from "next/image";
 
 export default function Page() {
-  const basePath = "/blog-preview-card-main/";
+  const basePath = "/blog-preview-card-main";
 
   return (
     <main className="flex h-screen items-center justify-center">
       <section className="w-1/3 rounded-lg bg-white shadow-xl shadow-black">
-        {/* 画像 */}
         <Image 
           src={`${basePath}/illustration-article.svg`}
           width={336}
@@ -14,14 +13,20 @@ export default function Page() {
           alt="illustration-editor-desktop"
           className="rounded-lg m-3"
         />
-        {/* type (ex. learning)  */}
-        {/* date */}
+        <h2>Leaning</h2>
+        <p>Published 21 Dec 2023</p>
         <h1>HTML & CSS foundations</h1>
         <p>
           These languages are the backbone of every website, defining structure,
           content, and presentation.
         </p>
-        {/* アイコン */}
+        <Image 
+          src={`${basePath}/image-avatar.webp`}
+          width={64}
+          height={64}
+          alt="image-avatar"
+          className="m-3"
+        />
       </section>
     </main>
   );
