b92e77b780c6935f83332ca4d174dcdc5cb2d919
Ryo Nakai <50983271+sniper-fly@users.noreply.github.com>
2024-08-04 17:29:25 +0900

    一通りのテンプレ要素を配置

--- src/app/product-preview-card-component-main/page.tsx
+++ src/app/product-preview-card-component-main/page.tsx
@@ -11,7 +11,7 @@ export default function Page() {
 
   return (
     <div className="flex h-screen items-center justify-center">
-      <main className="flex flex-row rounded-2xl">
+      <main className="flex flex-row rounded-2xl max-w-xl">
         {/* 画像を半分配置 */}
         <Image
           src={`${basePath}/image-product-desktop.jpg`}
@@ -21,7 +21,23 @@ export default function Page() {
           className="w-1/2"
         />
         {/* 文章とボタンの配置 */}
-        <article className="w-1/2 bg-white">hoge</article>
+        <article className="w-1/2 bg-white">
+          <h2>PERFUME</h2>
+          <h1>
+            <div>Gabrielle</div>
+            <div>Essence Eau</div>
+            <div>De Parfum</div>
+          </h1>
+          <section>
+            <p>
+              A floral, solar and voluptuous interpretation composed by Olivier
+              Polge, Perfumer-Creator for the House of CHANEL.
+            </p>
+          </section>
+          <h1>$149.99</h1>
+          <h2>$169.99</h2>
+          <button>Add to Cart</button>
+        </article>
       </main>
     </div>
   );
